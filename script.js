f92b2c10e67f60d60fa27ac51f6ed4e5da6e426e
James Aules <james_aules@icloud.com>
2023-03-04 12:30:46 -0500

    adding li/ render data

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const list = document.getElementById('list')
 const clearBtn = document.getElementById('clear')
 
 //instatiate default state value:
-const instatiate = {
+const initialState = {
     groceries: []
 }
 
@@ -33,4 +33,45 @@ const clearList = () => {
     store.dispatch({
         type: 'grocery/clear'
     })
+    console.log(store.getState())
 }
+
+const newGrocery = (e) => {
+    e.preventDefault()
+    let groceryText = document.getElementById('newItem').value
+    store.dispatch({
+        type: 'grocery/add',
+        text: groceryText
+    })
+    console.log(store.getState())
+}
+
+grocerySubmit.addEventListener('click', (e) => {
+    newGrocery(e) 
+    document.getElementById('newItem').value = ''
+})
+
+clearBtn.addEventListener('click', clearList)
+
+const renderList = (state) => {
+    while(list.firstChild) {
+        list.removeChild(list.firstChild)
+    }
+    state.forEach(grocery => {
+        //generate a new list element for each property
+        let li = document.createElement('li')
+        //append to the DOM
+        list.appendChild(li)
+        //populate the text content of the list item
+        li.textContent = grocery.text
+    });
+}
+
+const render = () => {
+    const state = store.getState()
+    renderList(state)
+}
+
+store.subscribe(render)
+
+
